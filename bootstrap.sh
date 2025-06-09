83e595aa6f80a0c0f17fa20ddbb22add4ffa8c52
Bill Wheatley <bill_wheatley@mailcan.com>
2025-06-08 23:18:07 -0400

    Intial Playbooks

---
+++ bootstrap.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# For Open Mandriva repo file editing
+safe_edit_repo_file() {
+   grep -n "enabled" $1 |grep $2 >/dev/null
+   if [ $? -eq 0 ]; then
+       sed_exp=`echo $2`"s/.*/enabled=1/"
+       sudo sed -i $sed_exp $1 
+   else
+       echo Unexpected Repo file format [$1], expecting enabled flag on line $2
+       echo ERROR: Script can not enable required Repo, this script needs to be fixed
+       exit 1
+   fi
+}
+
+if [ -x "$(command -v dnf)" ]; then 
+    if [ -f /etc/openmandriva-release ]; then
+        echo OpenMandrivia Detected, additional repos need to be turned on now
+        if [ -f /etc/yum.repos.d/openmandriva-rolling-x86_64.repo ]; then
+            echo Detected x86_64 Variant
+            repo_file=/etc/yum.repos.d/openmandriva-rolling-x86_64.repo
+        elif [ -f /etc/yum.repos.d/openmandriva-rolling-znver1.repo ]; then
+            echo Detected AMD Variant
+            repo_file=/etc/yum.repos.d/openmandriva-rolling-znver1.repo
+        fi
+        sudo cp $repo_file $repo_file.backup
+        echo Backup of repo file made if this fails: $repo_file.backup
+        # Doing this by Line number
+        safe_edit_repo_file $repo_file "59"
+        safe_edit_repo_file $repo_file "107"
+        safe_edit_repo_file $repo_file "155"
+
+        # OM uses bsd-tar by default, Ansible needs gnu-tar, this will fix this (and it's not an official dependency of ansible in OM repos)
+        sudo dnf -y install gnutar ansible git sshpass
+    else
+        echo Not the expected distro, looking for OpenMandrivia
+        exit 1        
+    fi
+else 
+    echo Not sure which package manager is running on this machine
+    exit 1
+fi
+
+#clone from GIT
+git clone https://github.com/billwheatley/provision-family-desktop.git
+
+# Make sure plain 'python' is in path (ansible does not do will without it)
+if [ ! `which python` ]; then
+  PYTHON3_LOC=$(which python3)
+  PYTHON_LOC=$(dirname $PYTHON3_LOC)/python
+  echo Need to Link $PYTHON3_LOC $PYTHON_LOC [using sudo]
+  sudo ln -s $PYTHON3_LOC $PYTHON_LOC
+fi
+
+# Make Roles-dir
+mkdir desktop-roles
+
+# Make automated install dirs
+mkdir -p $HOME/automated-install/
+
+#Call key setup playbook
+PRIMARY_USER_GROUP=`id -gn`
+cd provision-desktop
+
+#Need sudo on open-mandriva because facts plugin needs it
+sudo ansible-playbook key-setup.yaml --extra-vars="localhost_user=${USER} localhost_user_group=${PRIMARY_USER_GROUP} ansible_user_dir=${HOME}"
+
---
+++ provision-localhost.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+function show_help {
+    echo Provision your OpenMandriva or fedora desktops
+    echo 
+    echo OPTIONS:
+    echo "   -p      Skip Package Manager Updates (does not disable updates from roles)"
+    echo "   -r      Skip Role Definition Updates"
+    echo "   -s      Skip Snap Package Install/Updates (useful if store is down)"
+}
+
+# Function to add or update the skip tag list
+function add_skip_tag {
+    if [ -z ${skip_tags} ]; then
+        skip_tags="$1"
+    else
+        skip_tags="$skip_tags,$1"    
+    fi
+}
+
+while getopts "hprs" opt; do
+    case "$opt" in
+    h)
+        show_help
+        exit 0
+        ;;
+    p)  
+        add_skip_tag "pkg_update"
+        ;;
+    r)  
+        role_updates="skip"
+        ;;
+    s)  
+        add_skip_tag "snap_pkgs"
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+[ "${1:-}" = "--" ] && shift
+
+DIRNAME=`dirname "$0"`
+
+LOCAL_USER=${USER}
+
+# Determine skip tags
+if [ ! -z ${skip_tags} ]; then
+    skip_tag_directive="--skip-tags $skip_tags"
+fi
+
+#Update roles if not skipped 
+if [ ! "$role_updates" == "skip" ]; then
+    ansible-galaxy install -f -r requirements.yaml
+fi
+
+sudo ansible-playbook provision.yaml -v --extra-vars="for_user=${LOCAL_USER}" ${skip_tag_directive}
